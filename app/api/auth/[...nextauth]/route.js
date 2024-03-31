015e983ebb0518b614e664099092870a8e9752bb
Ragul8775 <rb7439@srmist.edu.in>
2024-04-01 00:59:34 +0530

    mongoAtlas and google api is done

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth/next";
+import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      //serverless -> Lambda -> dynamodb
+      await connectToDB();
+      //check if user already exist
+
+      //if not , create a new user and save it in database
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+export { handler as GET, handler as POST };
